212fdf8fa7c21d735a59c66703bf450c7c7bdf27
Alex <aleksandar.lokoski@gmail.com>
2021-07-16 23:16:28 -0400

    changing the pomodoro timer logic

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -10,40 +10,65 @@ export default function PomodoroTimer() {
   const timerMins = mins < 10 ? `0${mins}` : mins;
   const timerSecs = secs < 10 ? `0${secs}` : secs;
 
-  useEffect(() => {
-    let interval = setInterval(() => {
-      clearInterval(interval);
-
-      if (secs === 0) {
-        if (mins !== 0) {
-          setSecs(59);
-          setMins(mins - 1);
-        } else {
-            let mins = message ? 1 : 1;
+  const handleStart = () =>{
+    if(secs === 0) {
+        if(mins !== 0){
+            setSecs(59);
+            setMins(mins-1);
+        }else{
+            let mins = message ? 25 : 5;
 
             setMins(mins);
             setMessage(false);
-            setCycles(cycles + 1);
-            setCyclesMessage(false);
+            setCycles(cycles + 1)
         }
-      } else {
-        setSecs(secs - 1);
-      }
+    }else{
+        setSecs(secs-1)
+    }
+}
 
-      if (cycles === 4) {
-        let mins = cyclesMessage ? 2 : 1;
+const handleReset = () =>{
+    setMins(25);
+    setSecs(0)
+}
 
-        setMessage(false);
-        setCycles(0);
-        setCyclesMessage(true);
-        setMins(mins);
-      } 
-      
-    }, 1000);
-  }, [mins, secs, message, cycles, cyclesMessage]);
+//   useEffect(() => {
+//     let interval = setInterval(() => {
+//       clearInterval(interval);
+
+//       if (secs === 0) {
+//         if (mins !== 0) {
+//           setSecs(59);
+//           setMins(mins - 1);
+//         } else {
+//             let mins = message ? 1 : 1;
+
+//             setMins(mins);
+//             setMessage(false);
+//             setCycles(cycles + 1);
+//             setCyclesMessage(false);
+//         }
+//       } else {
+//         setSecs(secs - 1);
+//       }
+
+//       if (cycles === 4) {
+//         let mins = cyclesMessage ? 2 : 1;
+
+//         setMessage(false);
+//         setCycles(0);
+//         setCyclesMessage(true);
+//         setMins(mins);
+//       } 
+
+//     }, 1000);
+//   }, [mins, secs, message, cycles, cyclesMessage]);
+
+    
 
   return (
     <div className="pomodoro-container">
+        
       <div className="message">
         {message && <h1>Take 5! New session in:</h1>}
       </div>
@@ -59,6 +84,8 @@ export default function PomodoroTimer() {
       <div className="cycles">
         <h1>Cycles: {cycles}</h1>
       </div>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
