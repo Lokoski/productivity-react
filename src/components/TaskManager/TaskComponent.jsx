f51ae60eb712fb81d2425ba7764a6537def4cd9c
Alex <aleksandar.lokoski@gmail.com>
2021-07-24 23:51:48 -0400

    working on logic

--- src/components/TaskManager/TaskComponent.jsx
+++ src/components/TaskManager/TaskComponent.jsx
@@ -8,32 +8,88 @@ export default function TaskComponent() {
 
 
 
-    useEffect(() => {},[])
+    useEffect(() => {
+        loadFromLocalStorage();
+    },[])
+
+    const addEmptyTask = (status) =>{
+        const lastTask = tasks[tasks.length - 1];
+        let newTaskId = 1;
+
+        if(lastTask !== undefined){
+            newTaskId = lastTask.id + 1
+        }
+
+        setTasks(tasks => [
+            ...tasks, {
+                id: newTaskId,
+                title: "",
+                description: "",
+                urgency: "",
+                status: status
+            }
+        ])
+    }
 
     const addTask = (taskToAdd) =>{
+        let filteredTasks = tasks.filter((task) =>{ 
+            return task.id !== taskToAdd.id;
+        })
 
+        let newTaskList=[...filteredTasks, taskToAdd];
+
+        setTasks(newTaskList);
+
+        saveToLocalStorage(newTaskList);
     }
 
     const deleteTask = (taskId) =>{
+        let filteredTasks= tasks.filter((task) => {
+            return task.id !== taskId;
+        })
 
+        setTasks(filteredTasks)
+
+        saveToLocalStorage(filteredTasks);
     }
 
-    const moveTask = (taskId) =>{
+    const moveTask = (id, newStatus) =>{
+        let task = tasks.filter((task) => {
+            return task.id === id;
+        })
 
+        let filteredTasks = tasks.filter((task) => {
+            return task.id !== id;
+        })
+
+        task.status = newStatus;
+
+        let newTaskList = [...filteredTasks, task];
+
+        setTasks(newTaskList)
+
+        saveToLocalStorage(newTaskList);
     }
 
-    const saveToLocalStorage = () =>{
-
+    const saveToLocalStorage = (tasks) =>{
+        localStorage.setItem("tasks", JSON.stringify(tasks))
     }
 
     const loadFromLocalStorage = () =>{
+        let loadedTasks = localStorage.getItem("tasks");
 
+        let tasks = JSON.parse(loadedTasks);
+
+        if(tasks){
+            setTasks(tasks)
+        }
     }
 
   return <div className="task-container">
       <h1>Task Manager</h1>
       <Status 
           tasks={tasks}
+          addEmptyTask={addEmptyTask}
           addTask={addTask}
           deleteTask={deleteTask}
           moveTask={moveTask}
@@ -41,6 +97,7 @@ export default function TaskComponent() {
       />
        <Status 
           tasks={tasks}
+          addEmptyTask={addEmptyTask}
           addTask={addTask}
           deleteTask={deleteTask}
           moveTask={moveTask}
@@ -48,6 +105,7 @@ export default function TaskComponent() {
       />
        <Status 
           tasks={tasks}
+          addEmptyTask={addEmptyTask}
           addTask={addTask}
           deleteTask={deleteTask}
           moveTask={moveTask}
--- src/components/TaskManager/Tasks.jsx
+++ src/components/TaskManager/Tasks.jsx
@@ -9,24 +9,114 @@ const Tasks = (props) => {
 
     const [formAction, setFormAction] = useState('');
 
-    const setUrgency = (event) => {
-        setUrgencyLvl(event.target.attributes.urgency.value)
+    const setUrgency = (e) => {
+        setUrgencyLvl(e.target.attributes.urgency.value)
     }
 
-    const handleSubmit = (event) => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        if(formAction === "save") {
+            if(collapsed){
+                setCollapsed(false);
+            }else{
+                let newTask = {
+                    id: task.id,
+                    title: e.target.elements.title.value,
+                    description: e.target.elements.description.value,
+                    urgency: urgencyLvl,
+                    status: task.status,
+                    isCollapsed: true
+                }
+
+                addTask(newTask);
+                setCollapsed(true)
+            }
+        }
+
+        if(formAction === "delete"){
+            deleteTask(task.id)
+        }
     }
 
     const moveLeft = () => {
+        let newStatus = "";
 
+        if(task.status === "In Progress"){
+            newStatus = "Backlog"
+        }else if(task.status === "Completed"){
+            newStatus = "In Progress"
+        }
+        if(newStatus !== ""){
+            moveTask(task.id, newStatus)
+        }
     }
 
     const moveRight = () => {
-        
+        let newStatus = "";
+
+        if(task.status === "Backlog"){
+            newStatus = "In Progress"
+        }else if(task.status === "In Progress"){
+            newStatus = "Completed"
+        }
+        if(newStatus !== ""){
+            moveTask(task.id, newStatus)
+        }
     }
     return (
-        <div>
-            
+        <div className={`task ${collapsed ? "collapsedTask" : ""}`}>
+            <button className="button moveTask" onClick={moveLeft}>&#171;</button>
+            <form onSubmit={handleSubmit} className={collapsed ? "collapsed" : ""}>
+                <input 
+                    type="text"
+                    className="title input"
+                    name="title"
+                    placeHolder="Enter Title"
+                    disabled={collapsed}
+                    defaultValue={task.title}
+                />
+                <textarea
+                    rows = "2"
+                    className="description input"
+                    name="description"
+                    placeHolder="Enter Description"
+                    defaultValue={task.description}
+                 />
+                 <div>
+                     <label className={`low ${urgencyLvl === "low" ? "selected" : ""}`}>
+                     <input
+                        urgency="low"
+                        onChange={setUrgency}
+                        type="radio"
+                        name="urgency"
+                      />low
+                      </label>
+                    <label className={`medium ${urgencyLvl === "medium" ? "selected" : ""}`} >
+                     <input
+                        urgency="medium"
+                        onChange={setUrgency}
+                        type="radio"
+                        name="urgency"
+                    />low
+                    </label>
+                    <label className={`high ${urgencyLvl === "high" ? "selected" : ""}`} >
+                     <input
+                        urgency="high"
+                        onChange={setUrgency}
+                        type="radio"
+                        name="urgency"
+                      />high
+                      </label>
+                 </div>
+                 <button className="button" onClick={() => {
+                     setFormAction("save")
+                 }}>{collapsed ? "Edit" : "Save"}</button>
+                 {collapsed && <button className="button delete"onClick={() => {
+                     setFormAction("delete")
+                 }}>X</button>}
+            </form>
+            <button className="button moveTask" onClick={moveRight}>&#187;</button>
         </div>
     )
 }
